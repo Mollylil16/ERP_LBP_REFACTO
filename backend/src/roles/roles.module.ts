dde7aa9a34986fe16f6186d0676726dedb496856
mollylil16 <brunellomepieu01@gmail.com>
2026-04-09 08:51:14 +0100

    mise a niveau bd

--- backend/src/roles/roles.module.ts
+++ backend/src/roles/roles.module.ts
@@ -6,9 +6,13 @@ import { Role } from './entities/role.entity';
 import { Permission } from '../permissions/entities/permission.entity';
 import { RolePermission } from '../permissions/entities/role-permission.entity';
 import { PermissionsGuard } from '../auth/guards/permissions.guard';
+import { AuditModule } from '../audit/audit.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role, Permission, RolePermission])],
+  imports: [
+    TypeOrmModule.forFeature([Role, Permission, RolePermission]),
+    AuditModule,
+  ],
   controllers: [RolesController],
   providers: [RolesService, PermissionsGuard],
   exports: [RolesService, PermissionsGuard],
