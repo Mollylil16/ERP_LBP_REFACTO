82630d43b9fb90606cc064fcb23c755888a56557
pytharchimede <amani_ulrich@outlook.fr>
2026-06-14 20:43:12 +0000

    Correction CSS + redirection après refus d'accès

--- app/Middleware/PermissionMiddleware.php
+++ app/Middleware/PermissionMiddleware.php
@@ -29,15 +29,29 @@ class PermissionMiddleware
     {
         AuthMiddleware::check();
         if (!Auth::canOperation($operation)) {
-            self::deny('Vous ne disposez pas de tous les droits requis pour cette opération.');
+
+
+            $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+            $host = $_SERVER['HTTP_HOST'];
+            $request_uri = $_SERVER['REQUEST_URI'];
+
+            $current_url = $protocol . $host . $request_uri;
+
+            self::deny('Vous ne disposez pas de tous les droits requis pour cette opération. page actuelle ');
         }
     }
 
     private static function deny(string $message): never
     {
         Session::flash('error', $message);
-        $config = require BASE_PATH . '/config/app.php';
-        header('Location: ' . rtrim($config['url'], '/') . '/selection_portail');
+
+        // 1. Vérifie si la page précédente existe dans l'historique du navigateur
+        $referer = $_SERVER['HTTP_REFERER'] ?? null;
+
+        // 2. Si le referer existe, on redirige dessus, sinon on retourne à la racine '/'
+        $redirect_url = $referer ?: '/';
+
+        header('Location: ' . $redirect_url);
         exit;
     }
 }
