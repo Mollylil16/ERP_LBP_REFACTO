f7dd2949cc17ba38dfe42304272558a434841ffc
mollylil16 <brunellomepieu01@gmail.com>
2026-06-28 14:52:38 +0100

    feat: make CI payroll engine pages completely dynamic based on repository data

--- app/View/Pages/Rh/PayrollEnginePage.php
+++ app/View/Pages/Rh/PayrollEnginePage.php
@@ -6,7 +6,19 @@ namespace App\View\Pages\Rh;
 
 final class PayrollEnginePage
 {
-    public function __construct()
-    {
+    /**
+     * @param array<int,array<string,mixed>> $contractRules
+     * @param array<int,array<string,mixed>> $lineItems
+     * @param array<string,mixed> $payrollSettings
+     * @param array<int,array<string,mixed>> $periods
+     * @param array<int,array<string,mixed>> $slips
+     */
+    public function __construct(
+        public readonly array $contractRules = [],
+        public readonly array $lineItems = [],
+        public readonly array $payrollSettings = [],
+        public readonly array $periods = [],
+        public readonly array $slips = [],
+    ) {
     }
 }
