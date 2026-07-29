43efe19f34d6fb1fd1ce5136ac6c03398cbb7669
mollylil16 <brunellomepieu01@gmail.com>
2026-07-29 16:07:38 +0100

    fix: use ->redirect in HomeController

--- app/Controllers/Core/HomeController.php
+++ app/Controllers/Core/HomeController.php
@@ -4,16 +4,15 @@ namespace App\Controllers\Core;
 
 use App\Controllers\BaseController;
 use App\Helpers\Auth;
-use App\Helpers\Response;
 
 class HomeController extends BaseController
 {
     public function index(): void
     {
         if (Auth::check()) {
-            Response::redirect('selection_portail');
+            $this->redirect('selection_portail');
         } else {
-            Response::redirect('login');
+            $this->redirect('login');
         }
     }
 }
