d9a83d349adca216a7cce9667713d7544a9f1648
mollylil16 <brunellomepieu01@gmail.com>
2026-09-22 23:32:24 +0100

    fix: ajout des alias de compatibilite RoleMiddleware dans App\Helpers et Database dans App\Database

---
+++ app/Database/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Database;
+
+use App\Models\Database as BaseDatabase;
+
+/**
+ * Alias de compatibilité pour App\Models\Database.
+ */
+class Database extends BaseDatabase
+{
+}
---
+++ app/Helpers/RoleMiddleware.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+use App\Middleware\RoleMiddleware as BaseRoleMiddleware;
+
+/**
+ * Alias de compatibilité pour App\Middleware\RoleMiddleware.
+ */
+class RoleMiddleware extends BaseRoleMiddleware
+{
+}
